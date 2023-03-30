a4a1da359dac62e00f833793d26a7c72ad018990
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2023-03-30 12:08:36 +0200

    Tested stripe working good information absorbed can use with reka after some understanding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
+
 
 function App() {
+  const publishableKey ='pk_test_51MqH3fCOumsDCIEuHDYMd7LQpLV4MtfBOH3MQlOkXQ94loQ8QosfAb5BygVLszcFaSKfkcPVZvqMBl8LfEEXaSy900FeCYfM5w';
+  const [product, setproduct] = useState({
+    name: 'Mpesu',
+    price: 18,
+  });
+
+  const priceForStripe = product.price*100;
+
+  const payNow = async token =>{
+    try {
+      const response = await axios({
+        url: 'http://localhost:5000/payment',
+        method: 'post',
+        data: {
+          amount:  product.price*100,
+          token
+        },
+      });
+
+      if(response.status === 200){
+        handleSuccess();
+      }
+    } catch (error) {
+      handleFailure();
+      // console.log('Your payment was succesfull..')
+    }
+  }
+
+  const handleSuccess = () => {
+    MySwal.fire({
+      icon: 'success',
+      title: 'Payment was successful',
+      time: 4000,
+    });
+  };
+  const handleFailure = () => {
+    MySwal.fire({
+      icon: 'error',
+      title: 'Payment was not successful',
+      time: 4000,
+    });
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Complete React and Stripe Payment Intergration</h2>
+      <p>
+        <span>Product: </span>{product.name}
+      </p>
+      <p>
+        <span>Price: </span>${product.price}
+      </p>
+      <StripeCheckout
+        stripeKey={publishableKey}
+        label="Pay Now"
+        name="Pay With Credit Card"
+        billingAddress
+        shippingAddress
+        amount={priceForStripe}
+        description={`Your total is $${product.price}`}
+        token={payNow}
+      />
     </div>
   );
 }
